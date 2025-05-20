0e7cf2963af4c258f68d5d8c922035a22926708b
Vitalii Novik <novickvitaliy@gmail.com>
2025-05-20 19:17:48 +0300

    feat: add update modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,99 @@
 import React, {useState} from 'react'
 import './App.css'
-import {Input, DatePicker, Button, Form,} from "antd";
+import {Input, DatePicker, Button, Form, Modal, Select,} from "antd";
 import type {DatePickerProps, FormProps} from "antd/lib";
 import dayjs from "dayjs";
+import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
 
 const {TextArea} = Input;
 
+let idRunner = 1;
+
+type TodoTaskStatus = "Todo" | "InProgress" | "Done"
+
 type TodoTask = {
+    id: number;
     name: string;
     description: string;
     dueDate: string;
+    status: TodoTaskStatus
 };
 
 function App() {
+    const [isUpdateTodoTaskModalOpen, setIsUpdateTodoTaskModalOpen] = useState(false);
+    const [todoTaskToUpdate, setTodoTaskToUpdate] = useState<TodoTask | null>(null);
     const [todos, setTodos] = useState<TodoTask[]>([]);
     const today = new Date().toISOString().split('T')[0];
 
-    const [form] = Form.useForm<TodoTask>();
+    const [createTodoTaskForm] = Form.useForm<TodoTask>();
+    const [updateTodoTaskForm] = Form.useForm<TodoTask>();
 
-    const onFinish: FormProps<TodoTask>['onFinish'] = (values) => {
-        const date = new Date(values.dueDate.toString()).toISOString().split('T')[0];
+    const onFinishCreateTodoTask: FormProps<TodoTask>['onFinish'] = (values) => {
+        const date = dayjs(values.dueDate).format('YYYY-MM-DD');
         const todoTask: TodoTask = {
             ...values,
-            dueDate: date
+            dueDate: date,
+            id: idRunner,
+            status: "Todo"
         };
+        idRunner += 1;
         setTodos(prevTodos => [...prevTodos, todoTask]);
-        form.resetFields();
+        createTodoTaskForm.resetFields();
     };
 
-    const onFinishFailed: FormProps<TodoTask>['onFinishFailed'] = (errorInfo) => {
-        console.log(errorInfo)
+    const onFinishUpdateTodoTask: FormProps<TodoTask>['onFinish'] = (values) => {
+        if(todoTaskToUpdate !== null){
+            console.log(values)
+            const date = dayjs(values.dueDate).format('YYYY-MM-DD');
+            console.log(date)
+            todoTaskToUpdate.name = values.name;
+            todoTaskToUpdate.dueDate = date;
+            todoTaskToUpdate.description = values.description;
+            const filteredTodos = todos.filter(x => x.id !== todoTaskToUpdate?.id);
+            setTodos([...filteredTodos, todoTaskToUpdate]);
+            updateTodoTaskForm.resetFields();
+            setIsUpdateTodoTaskModalOpen(false);
+            setTodoTaskToUpdate(null);
+        }
     };
+
+    const onDeleteTodoTask = () => {
+
+    };
+
+    const onEditTodoTask = (taskId: number) => {
+        const todo = todos.filter(t => t.id === taskId)[0];
+        if (todo){
+            updateTodoTaskForm.setFieldsValue({
+                name: todo.name,
+                description: todo.description,
+                dueDate: dayjs(todo.dueDate),
+                status: todo.status
+            });
+            setTodoTaskToUpdate(todo);
+            setIsUpdateTodoTaskModalOpen(true);
+        }
+    };
+
+
+    const handleUpdateTodoTaskOk = () => {
+        updateTodoTaskForm.submit();
+        setIsUpdateTodoTaskModalOpen(false);
+    }
+
+    const handleTodoTaskCancel = () => {
+        setIsUpdateTodoTaskModalOpen(false);
+    }
 
     return (
         <>
             <h1>(not) Amazing Todo List</h1>
             <div className='main'>
                 <div>
-
                     <Form
-                        form={form}
+                        form={createTodoTaskForm}
                         className='todo-form'
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
+                        onFinish={onFinishCreateTodoTask}
                         initialValues={{name: '', description: '', dueDate: ''}}
                         layout={"vertical"}
                     >
@@ -60,7 +111,8 @@ function App() {
                                 name='description'
                                 label={'Description'}
                             >
-                                <TextArea placeholder={'Put the description of your task here'} autoSize={{minRows: 2, maxRows: 4}} name='description'/>
+                                <TextArea placeholder={'Put the description of your task here'}
+                                          autoSize={{minRows: 2, maxRows: 4}} name='description'/>
                             </Form.Item>
                         </div>
                         <div className='form-group'>
@@ -81,18 +133,85 @@ function App() {
                     <div className={'todo-list__items'}>
                         <h4 style={{margin: '0px'}}>Your tasks:</h4>
                         {todos.map(t => (
-                            <div className={'todo-list__item'}>
+                            <div key={t.id} className={'todo-list__item'}>
                                 <div>
                                     {t.name} | Due To: {t.dueDate} | Status: Todo
                                 </div>
-                                <div>
-
+                                <div className={'todo-list__options'}>
+                                    <Button onClick={() => onEditTodoTask(t.id)}>
+                                        <EditOutlined/>
+                                    </Button>
+                                    <Button danger onClick={onDeleteTodoTask}>
+                                        <DeleteOutlined color={'danger'}/>
+                                    </Button>
                                 </div>
                             </div>
                         ))}
                     </div>
                 </div>
             </div>
+            <Modal
+                title="Update Todo Task"
+                closable={{'aria-label': 'Custom Close Button'}}
+                open={isUpdateTodoTaskModalOpen}
+                onOk={handleUpdateTodoTaskOk}
+                onCancel={handleTodoTaskCancel}
+            >
+                {todoTaskToUpdate && (<>
+
+                        <Form
+                            form={updateTodoTaskForm}
+                            className='todo-form'
+                            onFinish={onFinishUpdateTodoTask}
+                            layout={"vertical"}
+                        >
+                            <div className='form-group'>
+                                <Form.Item<TodoTask>
+                                    name='name'
+                                    label={'Name'}
+                                    rules={[{required: true, message: "Please give the name for your task"}]}
+                                >
+                                    <Input placeholder={'Enter todo task name'} name='name' value={todoTaskToUpdate.name}></Input>
+                                </Form.Item>
+                            </div>
+                            <div className='form-group'>
+                                <Form.Item<TodoTask>
+                                    name='description'
+                                    label={'Description'}
+                                >
+                                    <TextArea value={todoTaskToUpdate.description} placeholder={'Put the description of your task here'}
+                                              autoSize={{minRows: 2, maxRows: 4}} name='description'/>
+                                </Form.Item>
+                            </div>
+                            <div className='form-group'>
+                                <Form.Item<TodoTask>
+                                    label={'Due Date'}
+                                    name='dueDate'
+                                    rules={[{required: true, message: "Due Date is required"}]}
+                                >
+                                    <DatePicker name='dueDate' minDate={dayjs(today)} value={dayjs(todoTaskToUpdate.dueDate)}/>
+                                </Form.Item>
+                            </div>
+                            <div className='form-group'>
+                                <Form.Item<TodoTask>
+                                    label={'Status'}
+                                    name='status'
+                                >
+                                    <Select
+                                        defaultValue="Todo"
+                                        style={{ width: 120 }}
+                                        options={[
+                                            { value: 'Todo', label: 'Todo' },
+                                            { value: 'InProgress', label: 'In Progress' },
+                                            { value: 'Done', label: 'Done' },
+                                        ]}
+                                    />
+                                </Form.Item>
+                            </div>
+                        </Form>
+                    </>
+                )}
+            </Modal>
         </>
     )
 }
